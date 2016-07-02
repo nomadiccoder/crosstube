e8949f40e41623b355297d756bffbbdd2dd2e54c
boredMgr <bikashjpatra@gmail.com>
2016-07-03 00:09:50 +0530

    deleted:    client/common/header.js 	modified:   client/components/homePage.js

--- client/components/homePage.js
+++ client/components/homePage.js
@@ -3,6 +3,7 @@
 var React = require('react');
 var Router = require('react-router');
 var Header = require('./common/header');
+var VidoeList = require('./videos/videoList');
 var Link = Router.Link;
 
 var Home = React.createClass({
@@ -12,6 +13,7 @@ var Home = React.createClass({
 					<div className="jumbotron text-center">
 						<h2> Crossover Video Portal </h2>
 					</div>
+					<VideoList/>
 				</div>
 			);
 	}
