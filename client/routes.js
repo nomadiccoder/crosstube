90474bb7d34d88f7bd73a00fe48cd275bfb28bcc
boredMgr <bikashjpatra@gmail.com>
2016-07-02 21:06:23 +0530

    modified:   .gitignore 	new file:   client/routes.js

---
+++ client/routes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var React = require('react');
+
+var Router = require('react-router');
+
+var DefaultRoute = Router.DefaultRoute;
+
+var Redirect = Router.Redirect;
+
+var Route = Router.Route;
+
+var routes = (
+		<Route name="app" path="/" handler={require('./components/app')}>
+			<DefaultRoute handler={require('./components/homePage')} />
+			<Route name="video" handler={require('./components/articles/articlesPage')} />
+		</Route>
+	);
+
+module.exports=routes;
